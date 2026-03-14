ad8a469f0dd1f904e86290ef77b48bd968e22952
RobertF05 <rfuentesf05@gmail.com>
2026-03-13 21:43:57 -0600

    a

--- backend/firebaseClient.js
+++ backend/firebaseClient.js
@@ -6,6 +6,9 @@ const serviceAccount = {
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
 }
 
+console.log("Project:", process.env.FIREBASE_PROJECT_ID)
+console.log("Email:", process.env.FIREBASE_CLIENT_EMAIL)
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 })
