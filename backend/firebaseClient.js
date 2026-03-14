11c05dd40808c909c94af43bdd0e282b3d0cf913
RobertF05 <rfuentesf05@gmail.com>
2026-03-13 22:04:24 -0600

    Por favor

--- backend/firebaseClient.js
+++ backend/firebaseClient.js
@@ -1,14 +1,6 @@
 import admin from "firebase-admin"
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
-}
-
-console.log("PROJECT:", process.env.FIREBASE_PROJECT_ID)
-console.log("EMAIL:", process.env.FIREBASE_CLIENT_EMAIL)
-console.log("KEY EXISTS:", !!process.env.FIREBASE_PRIVATE_KEY)
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
