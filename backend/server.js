7911912d07570d9f7c39e4bdb943184cab8aba81
RobertF05 <rfuentesf05@gmail.com>
2026-03-13 21:34:32 -0600

    Cambios de Conexion con la BD

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import donationRoutes from './routes/donations.js'
 
+dotenv.config()
+
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 
 app.use('/api', donationRoutes)
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
 })
